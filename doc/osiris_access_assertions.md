16a9f307e89f4ddcb7e0712affdb40bf65e71d13
Michael Gregorowicz <mike@detcw.com>
2016-09-07 17:00:56 -0400

    early documentation some progress on fleshing out supporting libs

---
+++ doc/osiris_access_assertions.md
@@ -0,0 +1,84 @@
+# OSiRIS Access Assertions
+
+## Assertion Types
+* _OSiRIS Access Request (OAR)_ - Issued by _Central Authority_ `oakd` to one or more _Resource Authorities_ `stpd` to provision services and receive an...
+* _OSiRIS Access Grant (OAG)_ - Issued by _Resource Authorities_ `stpd` to the _Central Authority_ `oakd` to be included in an...
+* _OSiRIS Access Assertion (OAA)_ - To be stored by _Central Authority_ `oakd` and delivered to user agents as part of an..
+* _OSiRIS Access Token (OAT)_ (short lived, hours/days/weeks) and / or an _OSiRIS Refresh Token (ORT)_ (longer-lived, months/years) which are stored on client machines and are used as bearer tokens to gain access to resources
+
+## Authority Types
+
+* _Central Authority_ - e.g. OSiRIS `oakd`, trusts InCommon and other IdPs, handles Authentication and authorization for the OSiRIS enterprise itself, and performs some AuthZ.  If `oakd` issues an 
+
+* _Resource Authorities_ - e.g. OSiRIS `stpd`, has certificates and will sign tokens issued by _Identity Authorities_ if certain criteria are met.  This gives resource owners a bit of their own AuthZ power and might come in handy.  This also introduces a point in the OAA issuance flow for provisioning of resources themselves.  Even if access keys are ephemeral, the resources themselves (UNIX Accounts, sudo rights, filesystems, namespaces, and block devices) are not.
+
+## OAA Issuance Flow
+
+### Resource Assertion Request
+
+```
+{
+    "iss": "urn:MI-OSiRIS",
+    "jti": "b724c2c5-0a42-4d0f-9c69-c21e240ac20b",
+    "iat": 1473280947,
+    "exp": 1473280977,
+    "sub": ["ak1520@wayne.edu", "urn:MI-OSiRIS:comanage:1"],
+    "aud": [
+        "urn:MI-OSiRIS:AfRlV67YnPf-Q-8WpM23B7Ds7vlIJJER", "urn:MI-OSiRIS:hRiHEh-3fCe47Kg0UhMVjx1RRYVsdqDk"
+    ],
+
+    // session key encrypted with each resource provider's public key
+    // provided in the same order they are listed in 'aud', any sensitive
+    // data will be encrypted with this symmetric session key using
+    // crypto_stream_xor, with the nonce being the first 
+    // crypto_stream_NONCEBYTES of the sha256 hash of the "jti" string
+    "sk": [
+        "vr77Lv31xJXQPR0pNA63JC4",
+        "/02ZGeOePwriwiO6F3r4eok"
+    ],
+
+    // an array of access 
+    // know how to provision and grant
+    access: [
+        "read", "write", "admin",
+
+        // but if the 'access' is an object instead of a string consider
+        // it 'advanced', and include extra information to facilitate the
+        // grant
+        {
+            // the label is what we call this access
+            "label": "shell-account",
+
+            // if present it means only these RPs should consider this
+            // portion of the OAR
+            "aud": ["urn:MI-OSiRIS:hRiHEh-3fCe47Kg0UhMVjx1RRYVsdqDk"],
+            
+            // preflight run to see if resource has been provisioned, if this 
+            // code returns 1, then run "grant", if this code returns 0 then
+            // run "prov", if this code returns -1 then run "dprov"
+            "pchk": ["1+CeZgfwM5DdboGVlrHmK3rashzaLiJf4h7YdlVkdDo6FM0jT4l4K"],
+            
+            // what to run to provision access
+            "prov": "[zaURai/rvr77Lv31xJXQPR0pNA63JC4am0FS8/gimfv+1LcV"],
+            
+            // what to run to deprovision resource
+            "dprov": "[zaURai/rvr77Lv31xJXQPR0pNA63JC4am0FS8/gimfv+1LcV"],
+            
+            // what to run to grant ephemeral access to the resource
+            "grant": ["zaURai/rvr77Lv31xJXQPR0pNA63JC4am0FS8/gimfv+1LcV"]
+        }
+    ],
+    resources: [
+        'monhost:/some/path',
+    ]
+}
+```
+
+### Resource Assertion Grant
+```
+{
+    "iss": "urn:MI-OSiRIS:AfRlV67YnPf-Q-8WpM23B7Ds7vlIJJER",
+    "sub": "ak1520@wayne.edu",
+
+}
+```
---
+++ lib/OSiRIS/AccessAssertion/Util.pm
@@ -0,0 +1,187 @@
+package OSiRIS::AccessAssertion::Util;
+
+# Utility Class for OSiRIS Access Assertions
+# Authored by: Michael Gregorowicz
+#
+# Copyright 2016 Wayne State University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+=pod
+
+=head1 NAME
+
+OSiRIS::AccessAssertion - Class for parsing, validating, and managing OSiRIS Access
+Assertion (OAA) objects
+
+=head1 OVERVIEW
+
+OAAs are a form of L<JSON Web Token|https://jwt.io> that are signed by a central OSiRIS
+authority and instruct services running on registered OSiRIS resources to configure their
+resources accordingly.  The two primary users of this library are the B<St. Peter Daemon>
+which acts as the primary consumer of OAAs and the B<OSiRIS Access Keyring Daemon> which 
+issues, stores, and delivers OAAs at hopefully appropriate times.
+
+At this point OAAs are all RS256 JWTs.  Any JWT library should be able to parse and 
+validate an OAA, not sure what you'd do with it though.  Maybe grant us access to your 
+ultra fast resources?!  That'd be awesome.
+
+=head1 FUNCTIONS / EXPORTS
+
+=over 2
+
+=cut
+
+use Mojo::Base 'Mojo::Util';
+use Mojo::Util @Mojo::Util::EXPORT_OK;
+use Crypt::Digest qw/digest_data digest_data_hex digest_file digest_file_hex/;
+use MIME::Base64 qw/encode_base64url decode_base64url/;
+use Crypt::Sodium;
+use Exporter 'import';
+
+our @EXPORT_OK = (
+    @Mojo::Util::EXPORT_OK,
+    # crypto functions
+    qw/
+        new_crypto_stream_key crypto_stream_xor
+    /,
+    # encoding functions
+    qw/
+        a85_encode a85_decode b64u_encode b64u_decode harness unharness
+    /,
+    # random string generators
+    qw/
+        random_bytes random_hex random_a85 random_b64 random_b64u
+    /,
+)
+
+# Aliases
+monkey_patch(__PACKAGE__, 'b64u_encode', \&encode_base64url);
+monkey_patch(__PACKAGE__, 'b64u"decode', \&decode_base64url);
+
+sub random_bytes {
+    my ($length) = @_;
+    return randombytes_buf($length // 32);
+}
+
+sub random_hex {
+    my ($length) = @_;
+    return substr(unpack('H*', random_bytes($length)), 0, $length // 32);
+}
+
+sub random_a85 {
+    my ($length) = @_;
+    return substr(encode_a85(random_bytes($length)), 0, $length // 32);
+}
+
+sub random_b64 {
+    my ($length) = @_;
+    return substr(encode_base64(random_bytes($length), ''), 0, $length // 32);
+}
+
+sub random_b64u {
+    my ($length) = @_;
+    return substr(encode_base64url(random_bytes($length), ''), 0, $length // 32);
+}
+
+sub new_crypto_stream_key {
+    return encode_base64(randombytes_buf(crypto_stream_KEYBYTES), '');
+}
+
+sub a85_encode {
+    my ($in, $opt) = @_;
+
+    my $_space_no = unpack 'N', ' ' x 4;
+
+    my $compress_zero = exists $opt->{compress_zero} ? $opt->{compress_zero} : 1;
+    my $compress_space = $opt->{compress_space};
+
+    my $padding = -length($in) % 4;
+    $in .= "\0" x $padding;
+    my $out = '';
+
+    for my $n (unpack 'N*', $in) {
+        if ($n == 0 && $compress_zero) {
+            $out .= 'z';
+            next;
+        }
+        if ($n == $_space_no && $compress_space) {
+            $out .= 'y';
+            next;
+        }
+
+        my $tmp = '';
+        for my $i (reverse 0 .. 4) {
+            my $mod = $n % 85;
+            $n = int($n / 85);
+            vec($tmp, $i, 8) = $mod + 33;
+        }
+        $out .= $tmp;
+    }
+
+    $padding or return $out;
+
+    $out =~ s/z\z/!!!!!/;
+    substr $out, 0, length($out) - $padding
+}
+
+sub a85_decode {
+    my ($in) = @_;
+
+    for ($in) {
+        tr[ \t\r\n\f][]d;
+        s/z/!!!!!/g;
+        s/y/+<VdL/g;
+    }
+
+    my $padding = -length($in) % 5;
+    $in .= 'u' x $padding;
+    my $out = '';
+
+    for my $n (unpack '(a5)*', $in) {
+        my $tmp = 0;
+        for my $i (unpack 'C*', $n) {
+            $tmp *= 85;
+            $tmp += $i - 33;
+        }
+        $out .= pack 'N', $tmp;
+    }
+
+    substr $out, 0, length($out) - $padding
+}
+
+sub harness {
+    my ($b64, $type) = @_;
+    return undef unless $type;
+
+    # just make sure it's upper case
+    $type = uc($type);
+
+    my ($pos, @pem) = (0, "-----BEGIN $type-----");
+    while (my $string = substr($b64, $pos, 64)) {
+        push(@pem, $string);
+        $pos += 64;
+    }
+    push(@pem, "-----END $type-----");
+
+    return join("\n", @pem);
+}
+
+sub unharness {
+    my ($pem) = @_;
+    my $unpem = join('', split("\n", $pem));
+    $unpem =~ s/^-----[^-]+-----([^-]+).+$/$1/g;
+    return $unpem;
+}
+
+1;
