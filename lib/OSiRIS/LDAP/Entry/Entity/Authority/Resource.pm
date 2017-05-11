c52c1b7ce7f4652f1d60b90ecfb16947d76d5170
Michael Gregorowicz <mike@mg2.org>
2017-05-11 16:20:27 -0400

    wrong OBJECT_CLASS definition in Authority::Resource

--- lib/OSiRIS/LDAP/Entry/Entity/Authority/Resource.pm
+++ lib/OSiRIS/LDAP/Entry/Entity/Authority/Resource.pm
@@ -1,7 +1,7 @@
 package OSiRIS::LDAP::Entry::Entity::Authority::Resource;
 use Mojo::Base 'OSiRIS::LDAP::Entry::Entity';
 
-has OBJECT_CLASS => sub { 'osirisCentralAuthority' };
+has OBJECT_CLASS => sub { 'osirisResourceAuthority' };
 has BASE_DN => sub { 'ou=Resource, ou=Authorities, dc=osris, dc=org' };
 
 1; 
